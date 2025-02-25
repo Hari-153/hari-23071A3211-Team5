9df467c56ff97d6db4f5637f7730236f0b6accb9
Hari-153 <haricp09@gmail.com>
2025-02-25 14:49:58 +0530

    Create unix_server.c

---
+++ unix_server.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCKET_PATH "/tmp/chat_socket"
+
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_un addr;
+    char buffer[256];
+
+    // Create a Unix domain socket
+    if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    // Bind the socket to the address
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 1) == -1) {
+        perror("listen");
+        ex
